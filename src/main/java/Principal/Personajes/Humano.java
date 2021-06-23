76e347265bf319799031976bca57e3e3120de651
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-23 10:57:36 -0600

    Agregue la subClase Humano.java que hereda de heroe

---
+++ src/main/java/Principal/Personajes/Humano.java
@@ -0,0 +1,12 @@
+
+package Principal.Personajes;
+
+/*
+ * @author pabloluis
+ */
+public class Humano extends Heroe {
+
+    public Humano(String nombre) {
+        super(180, nombre, 50);
+    }
+}
