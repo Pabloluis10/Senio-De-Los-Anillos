d8e8d7d46ebd9c67deb54cc1083786aa5442bde9
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-23 00:10:33 -0600

    En Personaje.java Agregue el constructor los metodos getters y los metodos recibirDaño(), valorAtaque() y atacarRival()

--- src/main/java/Principal/Personajes/Personaje.java
+++ src/main/java/Principal/Personajes/Personaje.java
@@ -1,14 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Principal.Personajes;
 
-/**
- *
- * @author pabloluis
+import java.util.Random;
+
+/*
+  @author pabloluis
  */
-public class Personajes {
+public class Personaje {
+    
+    Random aleatorio = new Random();
+    //Atributos
+    protected int vida;
+    protected int nombre;
+    protected int armadura;
+    protected final int LIMITE_ATAQUE;
+    protected final int DADOS;
+    protected String [] figuraMuerto;
+
+    
+    public Personaje(int vida, int nombre, int armadura, int LIMITE_ATAQUE, int DADOS) {
+        this.vida = vida;
+        this.nombre = nombre;
+        this.armadura = armadura;
+        this.LIMITE_ATAQUE = LIMITE_ATAQUE;
+        this.DADOS = DADOS;
+    }
+    
+    //metodos
+    public int valorAtaque(int aumento){
+        int valorMasALto=0;
+        for(int i=0; i<this.DADOS; i++){
+            int aux = aleatorio.nextInt(LIMITE_ATAQUE+1);
+            if(aux > valorMasALto){//elegimos el valor más alto
+                valorMasALto = aux;
+            }
+        }
+        return valorMasALto;
+    }
+    
+    public void atacarRival(Personaje rival){
+        int potenciaOfenciva = valorAtaque(0);
+        if(potenciaOfenciva > rival.getArmadura()){
+            int daño = potenciaOfenciva - rival.getArmadura();
+            rival.recibirDaño(daño);
+        }
+    }
+    
+    public void recibirDaño(int daño){
+        vida -= daño;
+    }
+    //getters 
+    public int getVida() {
+        return vida;
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+    
+    public int getArmadura(){
+        return armadura;
+    }
+
+    public String[] getFiguraMuerto() {
+        return figuraMuerto;
+    }
+    
+    //metodos
+    
     
 }
