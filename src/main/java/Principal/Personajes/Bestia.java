2deef46c83ea5ca3bfe2f4f4e3b64c9bdacacd37
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-23 11:08:38 -0600

    Agregue iniciarFigura en Bestia.java

--- src/main/java/Principal/Personajes/Bestia.java
+++ src/main/java/Principal/Personajes/Bestia.java
@@ -5,5 +5,44 @@ package Principal.Personajes;
  */
 public class Bestia extends Personaje {
     
+    private String [] figura;
+
+    public Bestia(int vida, String nombre, int armadura) {
+        super(vida, nombre, armadura, 90, 1);
+        figura = iniciarFigura();
+    }
     
+    private String [] iniciarFigura(){
+        String [] Figura ={ "               |       |       ",
+                            "           -----------------   ",
+                            "          *                 *  ",
+                            "         *     0       0     * ",
+                            "        *          ?          *",
+                            "\\ \\   *                     *",
+                            " \\ \\   *                   * ",
+                            "  \\ \\   *                 *  ",
+                            "   \\ \\   -----------------   ",
+                            "    \\ \\      |      |        ",
+                            "     \\ \\------------------   ",
+                            "      \\ \\*****************   ",
+                            "       ------------------/     ",
+                            "           |   BESTIA   |      ",
+                            "           |            |      ",
+                            "           |            |      ",
+                            "           |            |      ",
+                            "           |____________|      ",
+                            "            |  |    |  |       ",
+                            "            |  |    |  |       ",
+                            "            |  |    |  |       ",
+                            "            |  |    |  |       ",
+                            "            |  |    |  |       ",
+                            "            |  |    |  |       ",
+                            "          .-*  |    |  *-.     ",
+                            "        -*_____|    |_____*-   ",};
+        return Figura;
+    }
+
+    public String[] getFigura() {
+        return figura;
+    }
 }
