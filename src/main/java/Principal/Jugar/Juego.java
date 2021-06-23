56c096bdde3d1b8e2344e42b03793f7971967e45
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-23 16:41:47 -0600

    Solucione errores

--- src/main/java/Principal/Jugar/Juego.java
+++ src/main/java/Principal/Jugar/Juego.java
@@ -30,6 +30,7 @@ public class Juego {
         System.out.println("========== Antes de iniciar el juego ingrese los siguientes datos ==========");
         System.out.print("\nCuantos heroes desea para el juego: ");
         cantHeroes = entrada.nextInt();
+        entrada.nextLine();
         System.out.print("Cuantas bestias desea para el juego: ");
         cantBestias = entrada.nextInt();
         heroes = new Heroe[cantHeroes];
@@ -44,59 +45,59 @@ public class Juego {
         System.out.println("3) Héroe Humano");
         System.out.println("4) Bestia Orco");
         System.out.println("5) Bestia Trasgo");
-        System.out.print("Ingrese opción:");
+        System.out.println("Ingrese opción:");
         int opcion;
         do{
             opcion = entrada.nextInt();
             if(opcion<1 || opcion>5){
-                System.out.println("Ingreso una opción incorrecta vuela a ingresar una opción");
+                System.out.println("\nIngreso una opción incorrecta vuela a ingresar una opción");
             }
         }while(opcion<1 || opcion >5);
         //verificamos que haya espacio libre para ingresar personaje
-        if(opcion>=1 || opcion<=3){
+        if(opcion>=1 && opcion<=3){
             if(posLibreHeroe >= heroes.length){
-                System.out.println("Ya no hay espacio para ingresar héroes");
+                System.out.println("\nYa no hay espacio para ingresar héroes");
                 return;
             }
         }else{
             if(posLibreBestia >= bestias.length){
-                System.out.println("Ya no hay espacio para ingresar bestias");
+                System.out.println("\nYa no hay espacio para ingresar bestias");
                 return;
             }
         }
-        
-        String nombre;
+        System.out.println("Ingrese un nombre para el personaje");
+        String nombre = entrada.nextLine();
         switch(opcion){
             case 1:
-                System.out.print("Ingrese un nombre para el héroe Elfo: ");
+                
                 nombre = entrada.nextLine();
                 heroes[posLibreHeroe] = new Elfo(nombre);
                 posLibreHeroe++;
                 break;
             
             case 2:
-                System.out.print("Ingrese un nombre para el héroe Hobbit: ");
+                System.out.println("Ingrese un nombre para el héroe Hobbit: ");
                 nombre = entrada.nextLine();
                 heroes[posLibreHeroe] = new Hobbit(nombre);
                 posLibreHeroe++;
                 break;
                 
             case 3:
-                System.out.print("Ingrese un nombre para el héroe Humano: ");
+                System.out.println("Ingrese un nombre para el héroe Humano: ");
                 nombre = entrada.nextLine();
                 heroes[posLibreHeroe] = new Humano(nombre);
                 posLibreHeroe++;
                 break;
                 
             case 4:
-                System.out.print("Ingrese un nombre para la bestia Orco: ");
+                System.out.println("Ingrese un nombre para la bestia Orco: ");
                 nombre = entrada.nextLine();
                 bestias[posLibreBestia] = new Orco(nombre);
                 posLibreBestia++;
                 break;
                 
             case 5:
-                System.out.print("Ingrese un nombre para la bestia Trasgo: ");
+                System.out.println("Ingrese un nombre para la bestia Trasgo: ");
                 nombre = entrada.nextLine();
                 bestias[posLibreBestia] = new Trasgo(nombre);
                 posLibreBestia++;
@@ -115,15 +116,22 @@ public class Juego {
         while(heroes[0] != null && bestias[0] != null){
             System.out.println("¡¡¡¡¡ ROUND "+contadorPeleas);
             for(int i=0; i<cantBatallas; i++){
-                System.out.println("Pela entre"+heroes[i].getNombre()+" y "+bestias[i].getNombre());
-                heroes[i].atacarRival(bestias[i]);
-                bestias[i].atacarRival(heroes[i]);
-                dibujarPelea(heroes[i], bestias[i]);
-                
+                if(heroes[i]!= null && bestias[i]!= null) {
+                    System.out.println("Pela entre: "+heroes[i].getNombre()+" y "+bestias[i].getNombre());
+                    heroes[i].atacarRival(bestias[i]);
+                    bestias[i].atacarRival(heroes[i]);
+                    dibujarPelea(heroes[i], bestias[i]);
+                }
             }
             eliminarMuerto();
             contadorPeleas++;
         }
+        if(heroes[0] != null){
+            System.out.println("¡¡¡¡¡¡¡¡¡¡ Felicidades los ganadores son los heéroes !!!!!!!!!!");
+        } else {
+            System.out.println("¡¡¡¡¡¡¡¡¡¡ Bestias han sido los ganadores !!!!!!!!!!");
+        }
+        
     }
     
     private void dibujarPelea(Heroe heroe, Bestia bestia){
--- src/main/java/Principal/Principal.java
+++ src/main/java/Principal/Principal.java
@@ -1,21 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Principal;
 
-/**
- *
+import Principal.Jugar.Juego;
+import java.util.Scanner;
+
+/*
  * @author pabloluis
  */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
+    //variable
+    private static Juego juego;
+    private static Scanner entrada = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        juego = new Juego();
+        System.out.println("\n\n========== BIENVENIDO A EL JUEGO DEL SEÑOR DE LOS AÑILLOS ==========");
+        menu();
+    }
+    
+    public static void menu(){
+        int opcion;
+        do{
+            System.out.println("\n==================== menú ====================");
+            System.out.println("1) Ingresar un personaje");
+            System.out.println("2) Iniciar Pelea");
+            System.out.println("3) Salir");
+            opcion = entrada.nextInt();
+            switch (opcion){
+                case 1:
+                    juego.ingresarPersonaje();
+                    break;
+                case 2:
+                    juego.pelear();
+                    break;
+                case 3:
+                    System.out.println("Ha finalizado el juego.");
+                    break;
+                default: 
+                    System.out.println("No ha ingresado ningúna opción valida");
+                    break;
+            }
+        } while (opcion != 3);
     }
     
 }
