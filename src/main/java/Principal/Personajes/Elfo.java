9aec1e1e469f50eb4113fdcccbe341f8f8308a0a
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-23 10:46:03 -0600

    Agregue Elfo.java

---
+++ src/main/java/Principal/Personajes/Elfo.java
@@ -0,0 +1,28 @@
+package Principal.Personajes;
+
+/*
+ * @author pabloluis
+ */
+public class Elfo extends Heroe{
+
+    public Elfo(int nombre) {
+        super(250, nombre, 50);
+    }
+    
+    
+    @Override
+    public void atacarRival(Personaje rival){
+        if(rival instanceof Orcos){//si ataca a un orco
+            int potenciaOfenciva = valorAtaque(10);
+            if(potenciaOfenciva > rival.getArmadura()){
+                int daño = potenciaOfenciva - rival.getArmadura();
+                if(rival.getVida()< daño){//si la vida es menor al daño que se le hara al rival
+                    daño = rival.getVida();//para dejar la vida en 0
+                }
+                rival.recibirDaño(daño);
+            }
+        }else {
+            super.atacarRival(rival);
+        }
+    }
+}
